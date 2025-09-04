18fea7133566620c2077ab76e8e47eb87a893e8b
Gangadhara K <gangadharak2003@gmail.com>
2025-09-04 16:54:41 +0530

    edit

--- Datapath and Controller/Multiplication Through Addition/Equality_checker.v
+++
@@ -1,5 +0,0 @@
-module eqz(in,out);
-  input in;
-  output out;
-  assign out=(in==0);
-  endmodule
--- Datapath and Controller/Multiplication Through Addition/Pipo.v
+++
@@ -1,9 +0,0 @@
-module Pipo(in,clk,ld,out);
-  input [15:0]in;
-  input ld,clk;
-  output reg [15:0]out;
-  always @(posedge clk) begin
-    if(ld)
-      out<=in;
-  end
-endmodule
